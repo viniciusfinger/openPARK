62e4d5611424fcb005bfafa0154c19cf453ad819
Vinícius Finger <fingervinicius@icloud.com>
2020-12-26 16:06:57 -0300

    Commit inicial - Gerado com Spring Initializr

---
+++ src/test/java/OpenPark/OpenParkApplicationTests.java
@@ -0,0 +1,13 @@
+package OpenPark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenParkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

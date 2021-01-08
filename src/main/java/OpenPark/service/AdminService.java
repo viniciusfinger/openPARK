095bb5c6ec7109dbbe31b7a84f5fa1eda81516fe
Vinícius Finger <fingervinicius@icloud.com>
2021-01-08 19:50:20 -0300

    Alteraao na tela de administracao

--- src/main/java/OpenPark/service/AdminService.java
+++ src/main/java/OpenPark/service/AdminService.java
@@ -1,10 +1,14 @@
 package OpenPark.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.ModelAndView;
 
+import OpenPark.model.CategoriaVeiculo;
 import OpenPark.model.ConfiguracoesEstacionamento;
+import OpenPark.repository.CategoriaRepository;
 import OpenPark.repository.ConfiguracoesEstacionamentoRepository;
 
 @Service
@@ -14,11 +18,16 @@ public class AdminService {
 	ConfiguracoesEstacionamentoRepository configuracoesRepository;
 	
 	@Autowired
+	CategoriaRepository categoriaRepository;
 	
 	public ModelAndView configuration() {
 		ModelAndView mv = new ModelAndView("admin/configuration");
+
 		ConfiguracoesEstacionamento configuracoes =  configuracoesRepository.findById(1l).orElse(null);
+		List<CategoriaVeiculo> categorias = categoriaRepository.findAll();
+		
 		mv.addObject("configuracoes", configuracoes);
+		mv.addObject("categorias", categorias);
 		return mv;
 	}
 }
